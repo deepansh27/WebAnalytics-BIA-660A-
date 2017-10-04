2f884bb1b368bebe315984ff0dc814e00b72b2e0
Deepansh Parab <dparab@stevens.edu>
2017-10-03 20:24:05 -0400

    Create webcounter.py

---
+++ week4/webcounter.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 19 18:35:45 2017
+
+@author: deepanshparab
+"""
+
+import re
+from nltk.corpus import stopwords
+import requests
+
+
+
+
+def run(url,w1,w2): 
+
+    freq={} # keep the freq of each word in the file 
+    w1=w1.lower()
+    w2=w2.lower()
+    word_set = set()
+    stopLex=set(stopwords.words('english')) # build a set of english stopwrods 
+
+    success=False# become True when we get the file
+
+    for i in range(5): # try 5 times
+        try:
+            #use the browser to access the url 
+            response=requests.get(url,headers = { 'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36', })    
+            success=True # success
+            break # we got the file, break the loop
+        except:# browser.open() threw an exception, the attempt to get the response failed
+            print ('failed attempt',i)
+     
+    # all five attempts failed, return  None
+    if not success: return None
+    
+    text=response.text# read in the text from the file
+ 
+    sentences=text.split('.') # split the text into sentences 
+	
+    for sentence in sentences: # for each sentence 
+
+        sentence=sentence.lower().strip() # loewr case and strip	
+        sentence=re.sub('[^a-z]',' ',sentence) # replace all non-letter characters  with a space
+		
+        words=sentence.split(' ') # split to get the words in the sentence 
+
+        for word in words: # for each word in the sentence 
+    
+            if word=='' or word in stopLex:continue # ignore empty words and stopwords 
+            else: freq[word]=freq.get(word,0)+1
+
+    w1_counter = 0 
+    w2_counter = 0
+    
+    for value in freq.items():
+       
+        
+        if value[0] == w1:
+            w1_counter=value[1]
+        
+        if value[0] == w2:
+            w2_counter=value[1]
+    
+    for i in freq:
+        if freq[i]>w1_counter and freq[i]<w2_counter:
+            word_set.add(i)
+        
+    
+    
+    return word_set
+            
+    
+   
+            
+          
+
+
+
+if __name__=='__main__':
+    print(run('http://tedlappas.com/wp-content/uploads/2016/09/textfile.txt','Enormous','Amazon'))
